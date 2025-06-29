47431587cd7bc759dde4ae2bc4109b2c66359cab
chittalpatel <chittalkumar@gmail.com>
2025-06-29 18:07:52 +0100

    remove stock list from algo page

--- app.py
+++ app.py
@@ -28,15 +28,16 @@ def stock():
 
 @app.route('/run', methods=['GET', 'POST'])
 def run():
+    global stocks
+    _stocks = stocks
     result = []
-    history = 10
+    history = 200
     margin = 20
     last_close_margin = 5
     filter_by_last_close = True
     if request.method == 'POST':
         history = int(request.form["history"])
         margin = int(request.form["margin"])
-        _stocks = request.form["stocks"].splitlines()
         filter_by_last_close = bool(request.form.getlist("filter-by-last-close"))
         last_close_margin = int(request.form["last-close-margin"])
         for sname in _stocks:
@@ -46,9 +47,6 @@ def run():
                 print(f"Error occured while running algo {e}")
                 import traceback
                 traceback.print_exc()
-    else:
-        global stocks
-        _stocks = stocks
 
     return render_template("runAlgo.html", stocks="\n".join(_stocks), result=result or ["No results!"], history=history, margin=margin, last_close_margin=last_close_margin, filter_by_last_close=filter_by_last_close)
 
